ff10a8f92b354d27dda0a8c558fce632e87ddbc5
Abhishek Srivastava <abhishek.srivastava@aquevix.com>
2022-03-05 12:46:59 +0530

    Currect routing for product and order

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderComponent } from './order/order.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-  {path: '', component: ProductComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'order', component: OrderComponent},
 ];
 
 @NgModule({
