e8e5edd52e44bbb94d966a904672a6b7afa4eb7d
Abhishek Srivastava <abhishek.srivastava@aquevix.com>
2022-03-06 11:28:30 +0530

    Add Identity package and migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using API.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
         public DataContext(DbContextOptions options) : base(options)
         {
